4436f78e194a03fc52ed7eff19db494d34013423
Andrew Goodman <drewgoodman1@yahoo.com>
2019-02-20 18:22:10 -0600

    test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,14 @@
 
 $(document).ready(function() {
 
+  
   isPlaying = false;
   var heroSelected = false;
   var villainSelected = false;
   var currentHero;
   var currentVillain;
   var defeatedVillains = 0;
+  var wolfPic = "../"
 
 
 
@@ -33,14 +35,14 @@ $(document).ready(function() {
   };
 
   var bunceChar = {    
-    healthPoints : 100, 
+    healthPoints : 180, 
     attackPower : 10,
     counterAttack : 10,
     alive :  true
   };
 
   var boggisChar = {    
-    healthPoints : 100, 
+    healthPoints : 300, 
     attackPower : 10,
     counterAttack : 10,
     alive :  true
@@ -61,12 +63,10 @@ $(document).ready(function() {
 
       $("#foxButton").text("Attack");      
       $("#foxCard").attr("id","fightingHero");
-      $("#fightingHero").clone(true).appendTo("#heroSpot");
-
-      $("#fightingHero p:first" ).html("please work");
-    }
-
-    if(heroSelected && villainSelected){
+      
+      initializeHero();     
+    }    
+    if(heroSelected && villainSelected){      
       battle();
     }
   });
@@ -79,95 +79,107 @@ $(document).ready(function() {
 
       $("#mrsFoxButton").text("Attack");      
       $("#mrsFoxCard").attr("id","fightingHero");
-      $("#fightingHero").clone(true).appendTo("#heroSpot");
-    }
 
-    if(heroSelected && villainSelected){      
+      initializeHero();      
+    }
+    if(heroSelected && villainSelected){       
       battle();
     }
   });
 
-  $("#bunceButton").on("click", function() {
+  $("#krisButton").on("click", function() {
+
+    if(!heroSelected){
+      heroSelected = true;
+      currentHero = krisChar;
+
+      $("#krisButton").text("Attack");      
+      $("#krisCard").attr("id","fightingHero");
+
+      initializeHero();      
+    }
+    if(heroSelected && villainSelected){       
+      battle();
+    }
+  });
+
+  $("#bunceImage").on("click", function() {
       if(!villainSelected){
         villainSelected = true;
-        currentVillain = bunceChar;
-
-        $("#bunceButton").text("Defend");      
+        currentVillain = bunceChar;      
         $("#bunceCard").attr("id","fightingVillain");
-        $("#fightingVillain").clone(true).appendTo("#villainSpot");
+        initializeVillain();
       }
   });
 
-  $("#boggisButton").on("click", function() {
+  $("#boggisImage").on("click", function() {
       if(!villainSelected){
         villainSelected = true;
-        currentVillain = boggisChar;
-
-        $("#boggisButton").text("Defend");      
-        $("#boggisCard").attr("id","fightingVillain");
-        $("#fightingVillain").clone(true).appendTo("#villainSpot");
+        currentVillain = boggisChar;             
+        $("#boggisCard").attr("id","fightingVillain");        
+        initializeVillain();        
       }
   });
 
-  $("#beanButton").on("click", function() {
+  $("#beanImage").on("click", function() {
     if(!villainSelected){
       villainSelected = true;
-      currentVillain = beanChar;
-      
-      $("#beanButton").text("Enemy Selected");      
-      $("#beanCard").attr("id","fightingVillain");
-      $("#fightingVillain").clone(true).appendTo("#villainSpot");
-
-      
-      
+      currentVillain = beanChar;          
+      $("#beanCard").attr("id","fightingVillain")
+      initializeVillain();
     }  
 });
 
-  function battle(){ 
-    
-      
-            
+  function battle(){   
         currentHero.healthPoints -= currentVillain.counterAttack;
         currentVillain.healthPoints -= currentHero.attackPower;
-        currentHero.attackPower += currentHero.attackPower;      
+        currentHero.attackPower += currentHero.attackPower;  
 
-        //$("#fightingVillain p:first" ).html("Health: " + currentVillain.healthPoints);
-
-        console.log(currentHero);
-        console.log(currentVillain);
+        $("#heroSpot p:first").html("HP:  " + currentHero.healthPoints + "  Attack:  " + currentHero.attackPower);
+        $("#villainSpot p:first").html("HP:  " + currentVillain.healthPoints + "  Attack:  " + currentVillain.counterAttack);
           
-      if(currentVillain.healthPoints <= 0){
+        if(currentVillain.healthPoints <= 0){
+          currentVillain.alive = false;
+          currentVillain = {};        
+          villainSelected = false;
+          defeatedVillains++;        
 
-        currentVillain.alive = false;
-        currentVillain = {};        
-        villainSelected = false;
-        defeatedVillains++;
+          $("#villainSpot").empty();
+          $("#fightingVillain").removeAttr("id","fightingVillain");  
 
-        console.log(currentVillain);
-
-        $("#villainSpot").empty();
-        $("#fightingVillain").removeAttr("id","fightingVillain");  
-
-        if(defeatedVillains >2){
-          console.log("You Win");
+          if(defeatedVillains >2){            
+            showVictory();           
+          }        
         }
-        
-      }
-
       if(currentHero.healthPoints <= 0){
-
         currentHero.alive = false;
         currentHero = {};        
         heroSelected = false;
-        console.log(currentHero);
 
         $("#heroSpot").empty();
         $("#fightingHero").removeAttr("id","fightingHero");        
       }
   }
 
+  function initializeHero(){
+      $("#fightingHero p:first" ).html("HP:  " + currentHero.healthPoints + "  Attack:  " + currentHero.attackPower);
+      heroDiv = $("#fightingHero");      
+      $("#heroSpot").append(heroDiv);    
+  }
 
-      
+  function initializeVillain(){
+      $("#fightingVillain p:first" ).html("HP:  " + currentVillain.healthPoints + "  Attack:  " + currentVillain.counterAttack);
+      villainDiv = $("#fightingVillain");      
+      $("#villainSpot").append(villainDiv);    
+  }   
+  
+  function showVictory(){
+    console.log("cider");
+    victoryImage = "<img src='assets/images/black.gif' width='200px'/>";
+    
+    $("#battleSpot").html(victoryImage + "           You win!!");
+    
+  }
       
 }); 
 
